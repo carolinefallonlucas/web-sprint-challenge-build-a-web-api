dfa6b342fe0f21c1a221ec85a3baf48bada96c21
Caroline <carolinefallonlucas@gmail.com>
2021-01-17 17:02:33 -0500

    actions router and projects router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,18 @@
 // Write your "actions" router here!
+
+
+const router = require("express").Router()
+const Actions = require("./actions-model")
+
+router.get("/", (req, res) =>
+{
+    Actions.get()
+        .then(data =>
+        { 
+        res.status(200).json(data)
+        })
+    
+    .catch (err => {
+        res.status(500).json({err: err.message})
+    })
+})
